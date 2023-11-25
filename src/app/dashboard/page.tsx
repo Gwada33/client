12ab20aa007d13c13e9314f22fb00bab85bb9cf8
OpenLabsGit <graphiste.gwada@gmail.com>
2023-11-25 12:20:59 -0400

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,42 +1,119 @@
-import { LogoutButton } from "@/lib/LogoutButton";
-import { User } from "@/lib/User";
-import { getRequiredSession } from "@/lib/auth";
-
-import React from 'react';
-
-const LinkCard = ({ link }) => {
-  return (
-    <div className="max-w-sm bg-white rounded overflow-hidden shadow-lg mx-2 my-4">
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{link.title}</div>
-        <p className="text-gray-700 text-base">{link.url}</p>
-      </div>
-    </div>
-  );
-};
-
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { getRequiredSession } from "@/lib/auth"
 
 export default async function Dashboard() {
-  const links = [
-    { id: 1, title: 'GitHub', url: 'https://github.com/votre-username' },
-    { id: 2, title: 'LinkedIn', url: 'https://www.linkedin.com/in/votre-username' },
-    // Ajoutez d'autres liens au besoin...
-  ];
-  
   const session = await getRequiredSession()
-  return (<>
-    <h1>You are logged in</h1>
-    <LogoutButton />
-
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="flex flex-wrap justify-center">
-        {links.map((link) => (
-          <LinkCard key={link.id} link={link} />
-        ))}
-      </div>
-    </div>    
-    </>
+  return (
+    <div className="flex flex-col w-full min-h-screen bg-black text-white overflow-hidden">
+      <header className="flex items-center h-16 px-4 border-b border-[#333] md:px-6">
+        <Link className="flex items-center gap-2 text-lg font-semibold md:text-base" href="#">
+          <IconLink2 className="w-6 h-6 text-[#0ea5e9]" />
+          <span className="text-gradient bg-gradient-to-r from-[#0ea5e9] to-[#34D399]">LinkTree</span>
+        </Link>
+        <div className="flex items-center ml-auto gap-4 md:gap-2 lg:gap-4">
+          <Button className="rounded-full" size="icon" variant="ghost">
+            <img
+              alt="Avatar"
+              className="rounded-full"
+              height="32"
+              src="/placeholder.svg"
+              style={{
+                aspectRatio: "32/32",
+                objectFit: "cover",
+              }}
+              width="32"
+            />
+            <span className="sr-only">Toggle user menu</span>
+          </Button>
+        </div>
+      </header>
+      <main className="flex flex-row flex-1 min-h-[calc(100vh_-_theme(spacing.16))] p-4 md:p-10 bg-black">
+        <div className="flex flex-col gap-4 w-1/2">
+          <img
+            alt="Screen Visualization"
+            className="w-full rounded-lg"
+            height="500"
+            src="/placeholder.svg"
+            style={{
+              aspectRatio: "1000/500",
+              objectFit: "cover",
+            }}
+            width="1000"
+          />
+        </div>
+        <div className="flex flex-col gap-4 w-1/2">
+          <form className="flex flex-col gap-4 bg-black rounded-lg p-4">
+            <h2 className="text-2xl font-semibold text-gradient bg-gradient-to-r from-[#0ea5e9] to-[#34D399]">
+              Add New Link
+            </h2>
+            <input className="px-3 py-2 rounded text-white text-lg" placeholder="Link Title" type="text" />
+            <input className="px-3 py-2 rounded text-white text-lg" placeholder="Link URL" type="url" />
+            <Button className="px-4 py-2 rounded bg-[#0ea5e9] text-white text-lg" type="submit">
+              Add Link
+            </Button>
+          </form>
+          <div className="flex flex-col gap-4 bg-black rounded-lg p-4 mt-4 border-t-2 border-[#333]">
+            <h2 className="text-2xl font-semibold text-gradient bg-gradient-to-r from-[#0ea5e9] to-[#34D399]">Links</h2>
+            <div className="flex flex-col gap-2">
+              <a className="px-3 py-2 rounded bg-[#444] text-white flex items-center gap-2 text-lg" href="#">
+                <IconLink className="w-6 h-6 text-[#0ea5e9]" />
+                <span className="text-gradient bg-gradient-to-r from-[#0ea5e9] to-[#34D399]">Link Title</span>
+              </a>
+              <a className="px-3 py-2 rounded bg-[#444] text-white flex items-center gap-2 text-lg" href="#">
+                <IconLink className="w-6 h-6 text-[#0ea5e9]" />
+                <span className="text-gradient bg-gradient-to-r from-[#0ea5e9] to-[#34D399]">Link Title</span>
+              </a>
+              <a className="px-3 py-2 rounded bg-[#444] text-white flex items-center gap-2 text-lg" href="#">
+                <IconLink className="w-6 h-6 text-[#0ea5e9]" />
+                <span className="text-gradient bg-gradient-to-r from-[#0ea5e9] to-[#34D399]">Link Title</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
 
-// components/LinkCard.js
+function IconLink(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+      <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+    </svg>
+  )
+}
+
+
+function IconLink2(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M9 17H7A5 5 0 0 1 7 7h2" />
+      <path d="M15 7h2a5 5 0 1 1 0 10h-2" />
+      <line x1="8" x2="16" y1="12" y2="12" />
+    </svg>
+  )
+}
